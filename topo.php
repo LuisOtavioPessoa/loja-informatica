56af05b13256726efb7b88cbce16109c825853d2
LuisOtavioPessoa <otaviogame007@gmail.com>
2023-12-19 11:30:23 -0300

    Adicionando imagens dentro de botões

--- topo.php
+++ topo.php
@@ -38,6 +38,17 @@
   </div>
 </nav>
 
+<div class=" col-11 mx-auto">
+<button type="button" class="btn btn-outline-primary btn-lg"><img src="img/pcgamer.jpg" alt="pcgamer" width="200" height="150" ></button>
+<button type="button" class="btn btn-outline-secondary btn-lg"><img src="img/celular.jpg" alt="celular" width="200" height="150" ></button>
+<button type="button" class="btn btn-outline-success btn-lg"><img src="img/placavideo.jpg" alt="celular" width="200" height="150" ></button>
+<button type="button" class="btn btn-outline-danger btn-lg"><img src="img/ps4.png" alt="celular" width="200" height="150" ></button>
+<button type="button" class="btn btn-outline-warning btn-lg"><img src="img/jogo.jpg" alt="celular" width="200" height="150" ></button>
+</div>
+
+
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
